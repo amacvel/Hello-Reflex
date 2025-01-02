d79bf97c5089bcee753eaa76e9fc364defdfc880
Aitor Machado <aitor-g-bta@hotmail.com>
2025-01-02 22:02:36 +0000

    Dashboard app v2

--- Hello_Reflex/Hello_Reflex.py
+++ Hello_Reflex/Hello_Reflex.py
@@ -1,33 +1,103 @@
 import reflex as rx
 
+class User(rx.Base):
+    # The user model.
+    name: str
+    email: str
+    gender: str
+
 class State(rx.State):
-    users: list[list[str]] = [
-        ["Danilo Sousa", "danilo@example.com", "Male"],
-        ["Zahra Ambessa", "zahra@example.com", "Female"]
+    users: list[User] = [
+        User(
+            name="Danilo Sousa",
+            email="danilo@example.com",
+            gender="Male",
+        ),
+        User(
+            name="Zahra Ambessa",
+            email="zahra@example.com",
+            gender="Female",
+        ),
     ]
 
-def show_user(person: list):
+    def add_user(self, form_data: dict):
+        self.users.append(User(**form_data))
+
+def show_user(user: User):
     # Show a person in a table row.
     return rx.table.row(
-        rx.table.cell(person[0]),
-        rx.table.cell(person[1]),
-        rx.table.cell(person[2])
+        rx.table.cell(user.name),
+        rx.table.cell(user.email),
+        rx.table.cell(user.gender),
+    )
+
+def add_customer_button() -> rx.Component:
+    return rx.dialog.root(
+        rx.dialog.trigger(
+            rx.button(
+                rx.icon("plus", size=26),
+                rx.text("Add User", size="4"),
+            ),
+        ),
+        rx.dialog.content(
+            rx.dialog.title(
+                "Add New User",
+            ),
+            rx.dialog.description(
+                "Fill the form with the user's info",
+            ),
+            rx.form(
+                rx.flex(
+                    rx.input(
+                        placeholder="User Name",
+                        name="name",
+                        required=True,
+                    ),
+                    rx.input(
+                        placeholder="user@reflex.dev",
+                        name="email",
+                    ),
+                    rx.select(
+                        ["Male", "Female"],
+                        placeholder="Male",
+                        name="gender",
+                    ),
+                    rx.flex(
+                        rx.dialog.close(
+                            rx.button(
+                                "Submit", type="submit"
+                            ),
+                        ),
+                        spacing="3",
+                        justify="end",
+                    ),
+                    direction="column",
+                    spacing="4",
+                ),
+                on_submit=State.add_user,
+                reset_on_submit=False,
+            ),
+            max_width="450px",
+        ),
     )
 
 def index() -> rx.Component:
-    return rx.table.root(
-        rx.table.header(
-            rx.table.row(
-                rx.table.column_header_cell("Name"),
-                rx.table.column_header_cell("Email"),
-                rx.table.column_header_cell("Gender")
+    return rx.vstack(
+        add_customer_button(),
+        rx.table.root(
+            rx.table.header(
+                rx.table.row(
+                    rx.table.column_header_cell("Name"),
+                    rx.table.column_header_cell("Email"),
+                    rx.table.column_header_cell("Gender"),
+                ),
             ),
+            rx.table.body(
+                rx.foreach(State.users, show_user),
+            ),
+            variant="surface",
+            size="3",
         ),
-        rx.table.body(
-            rx.foreach(State.users, show_user)
-        ),
-        variant="surface",
-        size="3"
     )
 
 app = rx.App()
